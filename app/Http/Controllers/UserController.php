bf558d503071f4da36294dc3434282ada058cf82
AhmedMilad197 <ahmilad197@gmail.com>
2024-01-12 15:04:39 +0200

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ {
+    CreateUserRequest,
+    LoginUserRequest,
+};
+use App\Models\User;
+use Exception;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function index () {
+        return User::all();
+    }
+    
+    public function create (CreateUserRequest $createUserRequest) {
+        try {
+            $user = User::Create($createUserRequest->validated());
+            $token = $user->createToken('registration_token');
+            return [
+                'user' => $user,
+                'token' => $token->plainTextToken,
+            ];
+        } catch (Exception $error) {
+            return [
+                'error' => $error,
+            ];
+        }
+    }
+
+    public function login (LoginUserRequest $loginUserRequest) {
+        try {
+            $user = User::firstWhere('email', $loginUserRequest->email);
+            $response = [
+                'message' => 'These Credintials are not valid.',
+            ];
+            if ($user and Hash::check($loginUserRequest->password, $user->password)) {
+                $token = $user->createToken('login_token');
+                $response = [
+                    'user' => $user,
+                    'token' => $token->plainTextToken,
+                ];
+            }
+            return $response;
+        } catch (Exception $error) {
+            return [
+                'error' => $error,
+            ];
+        }
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/web', function () {
+    return "This message is from the web file";
+});
+
+Route::get('/csrf-token', function (Request $request) {
+    $token = $request->session()->token();
+ 
+    $token = csrf_token();
+ 
+    return $token;
+});
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+ 
+    return ['token' => $token->plainTextToken];
+});
