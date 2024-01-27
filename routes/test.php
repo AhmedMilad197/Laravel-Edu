c0ef435af88882579eb8c852284e2cdc20686139
AhmedMilad197 <ahmilad197@gmail.com>
2024-01-28 00:48:18 +0200

    spatie query builder example

--- routes/test.php
+++ routes/test.php
@@ -3,9 +3,24 @@
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Spatie\QueryBuilder\QueryBuilder;
 
+/**
+ * 
+ *  getting handy with laravel factory.
+ */
 Route::get('/factory', function () {
     return User::factory()
-            ->has(Post::factory()->count(3), 'posts')
-            ->create();
+        ->has(Post::factory()->count(3), 'posts')
+        ->create();
+});
+
+/**
+ * 
+ * use spatie query builder for filtering data.
+ */
+Route::get('/spatie/query-builder', function () {
+    return QueryBuilder::for(User::class)
+        ->allowedFilters(['name', 'email'])
+        ->get();
 });
